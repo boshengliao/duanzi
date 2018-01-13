cd4ac912cee6911c993ad82d5edf9b2b437572f0
bosheng <411425403@qq.com>
2018-01-13 12:24:36 +0800

    refactor JianDan

--- duanzi.py
+++ duanzi.py
@@ -6,53 +6,57 @@ from __future__ import unicode_literals
 import bs4
 import requests
 
-url = 'http://jandan.net/duan/page-303#comments'
-agent = ("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ("
-         "KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36")
-content_type = ''
-headers = {
-    'User-Agent': agent,
-}
 
-print agent
-r = requests.get(url, headers=headers)
-print r
-# print dir(r)
-# print 'text', r.text
-content = r.content
-# print 'content', content
-
-
-def find_author(element):
+class JianDan(object):
     """
-    找到该行的作者
+    爬取煎蛋网的段子
     """
-    i = element
-    t = i.select(".author > strong")
-    v = t[0]
-    name = v.string
-    return name
+    bs4 = bs4
+    requests = requests
+
+    url = 'http://jandan.net/duan/page-303#comments'
+    agent = ("Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 ("
+             "KHTML, like Gecko) Chrome/60.0.3112.101 Safari/537.36")
+    content_type = ''
+    headers = {
+        'User-Agent': agent,
+    }
+
+    def get_html(self, url):
+
+        headers = self.headers
+        f = self.requests.get
+        r = f(url, headers=headers)
+        print r
+        content = r.content
+        return content
+
+    def find_author(self, element):
+        """
+        找到该行的作者
+        """
+        i = element
+        t = i.select(".author > strong")
+        v = t[0]
+        name = v.string
+        return name
+
+    def get_soup(self, html, parser_type="html.parser"):
+        """
+        获取 soup
+        """
+        content = html
+        f = self.bs4.BeautifulSoup
+        soup = f(content, parser_type)
+        rows = soup.select(".row")
+        for i in rows:
+            name = self.find_author(i)
+            print name
+        return None
 
 
-soup = bs4.BeautifulSoup(content, "html.parser")
-rows = soup.select(".row")
-print type(soup), type(rows)
-for i in rows:
-    t = i.select(".author > strong")
-    v = t[0]
-    name = v.string
-    print name
-
-
-"""
-# 找到所有段子
-txts = soup.select(".text > p")
-# print 'txts', txts
-for i in txts:
-    print i
-"""
-
-
-def t():
-    print 'hello'
-    return None
+if __name__ == '__main__':
+    jd = JianDan()
+    url = jd.url
+    html = jd.get_html(url)
+    soup = jd.get_soup(html)
